11e9ec0be6d3d172da86cdf67125ddd932074b59
A. Demant <ademant@ademant.de>
2018-11-21 17:17:52 +0100

    code cleanup items.lua

--- items.lua
+++ items.lua
@@ -1,40 +1,12 @@
 ---------mob drops----------------
 
-minetest.register_craftitem('minerdream:slimeball_green', {
-		description = "Green ball of slime",
-	inventory_image = "minerdream_slimeball_green.png",
+for _,colo in pairs({"green","blue","red","yellow","purple","brown"}) do
+minetest.register_craftitem('minerdream:slimeball_'..colo, {
+		description = colo.." ball of slime",
+	inventory_image = "minerdream_slimeball_"..colo..".png",
 	stack_max= 1000,
 })
-
-minetest.register_craftitem('minerdream:slimeball_blue', {
-		description = "Blue ball of slime",
-	inventory_image = "minerdream_slimeball_blue.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:slimeball_red', {
-		description = "Red ball of slime",
-	inventory_image = "minerdream_slimeball_red.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:slimeball_yellow', {
-		description = "Yellow ball of slime",
-	inventory_image = "minerdream_slimeball_yellow.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:slimeball_purple', {
-		description = "Purple ball of slime",
-	inventory_image = "minerdream_slimeball_purple.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:slimeball_brown', {
-		description = "Brown ball of slime",
-	inventory_image = "minerdream_slimeball_brown.png",
-	stack_max= 1000,
-})
+end
 ---------------------materials---------------
 
 minetest.register_craftitem('minerdream:expensivestick', {
@@ -46,18 +18,6 @@ minetest.register_craftitem('minerdream:expensivestick', {
 minetest.register_craftitem('minerdream:steelstick', {
 		description = "Stainless steel stick",
 	inventory_image = "minerdream_steelstick.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:sulfur_powder', {
-		description = "Sulfur powder",
-	inventory_image = "minerdream_sulfur_powder.png",
-	stack_max= 1000,
-})
-
-minetest.register_craftitem('minerdream:saltpeter_powder', {
-		description = "Saltpeter powder",
-	inventory_image = "minerdream_saltpeter_powder.png",
 	stack_max= 1000,
 })
 
@@ -75,92 +35,12 @@ minetest.register_craftitem('minerdream:gold_nugget', {
 	stack_max= 1000,
 })
 ---------------refined goods-----------------
---[[
-minetest.register_craftitem('minerdream:rhodium_bar', {
-		description = "".. core.colorize("#00FF00", "Rhodium bar\n")..core.colorize("#A0A0A0", "tier: 6 (epic)"),
-	inventory_image = "minerdream_rhodium_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:tantalum_bar', {
-		description = "".. core.colorize("#00FF00", "Tantalum bar\n")..core.colorize("#A0A0A0", "tier: 6 (epic)"),
-	inventory_image = "minerdream_tantalum_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:ruthenium_bar', {
-		description = "".. core.colorize("#0040FF", "Ruthenium bar\n")..core.colorize("#A0A0A0", "tier: 5 (extra rare)"),
-	inventory_image = "minerdream_ruthenium_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:cobalt_bar', {
-		description = "".. core.colorize("#0040FF", "Cobalt bar\n")..core.colorize("#A0A0A0", "tier: 5 (extra rare)"),
-	inventory_image = "minerdream_cobalt_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:platinum_bar', {
-		description = "".. core.colorize("#40FF80", "Platinum bar\n")..core.colorize("#A0A0A0", "tier: 4 (rare)"),
-	inventory_image = "minerdream_platinum_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:aluminum_bar', {
-		description = "".. core.colorize("#40FF80", "Aluminum bar\n")..core.colorize("#A0A0A0", "tier: 4 (rare)"),
-	inventory_image = "minerdream_aluminum_bar.png",
-	stack_max= 250,
-})
-]]
-minetest.register_craftitem('minerdream:stainlesssteel_bar', {
-		description = "".. core.colorize("#BFFFFF", "Stainless steel bar\n")..core.colorize("#A0A0A0", "tier: 2 (uncommon)"),
-	inventory_image = "minerdream_stainlesssteel_bar.png",
-	stack_max= 250,
-})
-
 
 minetest.register_craftitem('minerdream:brass_bar', {
 		description = "".. core.colorize("#BFFFFF", "brass bar\n")..core.colorize("#A0A0A0", "tier: 2 (uncommon)"),
 	inventory_image = "minerdream_brass_bar.png",
 	stack_max= 250,
 })
---[[
-minetest.register_craftitem('minerdream:zinc_bar', {
-		description = "".. core.colorize("#BFFFFF", "zinc bar\n")..core.colorize("#A0A0A0", "tier: 2 (uncommon)"),
-	inventory_image = "minerdream_zinc_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:lead_bar', {
-		description = "".. core.colorize("#00FFFF", "lead bar\n")..core.colorize("#A0A0A0", "tier: 3 (almost rare)"),
-	inventory_image = "minerdream_lead_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:calcium_bar', {
-		description = "".. core.colorize("#FFFFFF", "Calcium bar\n")..core.colorize("#A0A0A0", "tier: 1 (common)"),
-	inventory_image = "minerdream_calcium_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:potassium_bar', {
-		description = "".. core.colorize("#FFFFFF", "Potassium bar\n")..core.colorize("#A0A0A0", "tier: 1 (common)"),
-	inventory_image = "minerdream_potassium_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:nickel_bar', {
-		description = "".. core.colorize("#BFFFFF", "Nickel bar\n")..core.colorize("#A0A0A0", "tier: 2 (uncommon)"),
-	inventory_image = "minerdream_nickel_bar.png",
-	stack_max= 250,
-})
-
-minetest.register_craftitem('minerdream:silver_bar', {
-		description = "".. core.colorize("#00FFFF", "Silver bar\n")..core.colorize("#A0A0A0", "tier: 3 (almost rare)"),
-	inventory_image = "minerdream_silver_bar.png",
-	stack_max= 250,
-})
-]]
 ------------------consumables----------------
 
 minetest.register_craftitem("minerdream:vitamin", {
@@ -176,7 +56,6 @@ minetest.register_craftitem("minerdream:potion_tiny", {
 	inventory_image = "minerdream_potion_tiny.png",
 	on_use = minetest.item_eat(5),
 })
-
 
 minetest.register_craftitem("minerdream:potion_lesser", {
 		description = "".. core.colorize("#FFFFFF", "Lesser healing potion\n")..core.colorize("#A0A0A0", "Consumable\n")..core.colorize("#A0A0A0", "Restores 10 hp"),
