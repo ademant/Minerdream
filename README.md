9ece20b30ae403e2192f83c09561e1d21fc1c3ab
ademant <ademant@ademant.de>
2018-12-04 07:17:23 +0100

    cleaning for modpack

--- README.md
+++
@@ -1,2 +0,0 @@
-# Minerdream_mod
-A mod, that makes minig way more fun, and is a replacement for Xtraores, which I abandoned.
--- steel_wire.lua
+++
@@ -1,6 +0,0 @@
-
-minetest.register_craftitem('minerdream:steel_wire', {
-		description = "Steel Wire",
-	inventory_image = "minerdream_steel_wire.png",
-	stack_max= 1000,
-})
