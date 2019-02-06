d529e42cd1a453be5b5355db999fd3a4bb9f5827
ademant <ademant@ademant.de>
2019-02-06 14:50:47 +0100

    add poor nodes and nuggets

--- md_awards/init.lua
+++ md_awards/init.lua
@@ -3,4 +3,7 @@ md_awards.path = minetest.get_modpath("md_awards")
 md_awards.modname = minetest.get_current_modname()
 
 dofile(md_awards.path .. "/awards.lua")
+if treasures ~= nil then
+	dofile(md_awards.path .. "/treasures.lua")
+end
 
